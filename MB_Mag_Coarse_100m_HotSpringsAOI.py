8f7fa41921cb9bad7f29ef5ab0607fb561bb7b45
D Fournier <fourndo@gmail.com>
2016-12-01 19:01:26 -0800

    Update script, add new obsfile with high uncertainties and output results

--- MB_Mag_Coarse_100m_HotSpringsAOI.py
+++ MB_Mag_Coarse_100m_HotSpringsAOI.py
@@ -8,7 +8,7 @@ Created on Thu Dec  1 10:40:47 2016
 import os
 import SimPEG.PF as PF
 from SimPEG import Maps, Regularization, Optimization, DataMisfit,\
-                   InvProblem, Directives, Inversion
+                   InvProblem, Directives, Inversion, Mesh
 from SimPEG.Utils.io_utils import remoteDownload
 import matplotlib.pyplot as plt
 import numpy as np
@@ -42,13 +42,14 @@ wgtexp = 3.
 H0 = (54318, 70.139, 16.063)
 
 # Start by importing files HOW DO I DO THIS??
-# DOM: Just point to your working directory where your input files are
-work_dir = 'C:\Somwhere\\'
-
+# DOM: Just point to your working directory where your input files are.
+# Since you move them to the current directory, it is just .\
+work_dir = ".\\"
+input_file = "MB_100m_input_file.inp"
 # %%
 # Read in the input file which included all parameters at once
 # (mesh, topo, model, survey, inv param, etc.)
-driver = PF.MagneticsDriver.MagneticsDriver_Inv(work_dir + MB_100m_input_file)
+driver = PF.MagneticsDriver.MagneticsDriver_Inv(work_dir + input_file)
 # %%
 # Now we need to create the survey and model information.
 
@@ -84,7 +85,7 @@ midx = int(mesh.nCx/2)
 # %%
 # Now that we have a model and a survey we can build the linear system ...
 # Create the forward model operator
-prob = PF.Gravity.GravityIntegral(mesh, mapping=staticCells,
+prob = PF.Magnetics.MagneticIntegral(mesh, mapping=staticCells,
                                   actInd=active)
 prob.solverOpts['accuracyTol'] = 1e-4
 
@@ -92,15 +93,18 @@ prob.solverOpts['accuracyTol'] = 1e-4
 survey.pair(prob)
 
 # Apply depth weighting
-wr = PF.Magnetics.get_dist_wgt(mesh, rxLoc, active, wgtexp,
-                               np.min(mesh.hx)/4.)
-wr = wr**2.
+# wr = PF.Magnetics.get_dist_wgt(mesh, rxLoc, active, wgtexp,
+#                                np.min(mesh.hx)/4.)
+# wr = wr**2.
+# DOM: I recommand just using the sensitivity for the MAG problem
+wr = np.sum(prob.G**2., axis=0)**0.5
+wr = (wr/np.max(wr))
 
 # %% Create inversion objects
 reg = Regularization.Sparse(mesh, indActive=active,
                             mapping=staticCells)
 reg.mref = driver.mref[dynamic]
-reg.cell_weights = wr * mesh.vol[active]
+reg.cell_weights = wr
 
 # Specify how the optimization will proceed
 opt = Optimization.ProjectedGNCG(maxIter=150, lower=driver.bounds[0],
@@ -128,6 +132,7 @@ IRLS = Directives.Update_IRLS(norms=driver.lpnorms, eps=driver.eps,
 # Preconditioning refreshing for each IRLS iteration
 update_Jacobi = Directives.Update_lin_PreCond()
 
+
 # Create combined the L2 and Lp problem
 inv = Inversion.BaseInversion(invProb,
                               directiveList=[IRLS, update_Jacobi, betaest])
@@ -150,104 +155,109 @@ L2out = activeMap * reg.l2model
 # reconstructing lp model mesh with air cells and active dynamic cells
 Lpout = activeMap*mrec
 
+Mesh.TensorMesh.writeModelUBC(mesh, work_dir + 'L2_model.sus', L2out)
+Mesh.TensorMesh.writeModelUBC(mesh, work_dir + 'Lp_model.sus', Lpout)
+
 # %%
 # Plot out sections and histograms of the smooth l2 model.
 # The ind= parameter is the slice of the model from top down.
-yslice = midx + 1
-L2out[L2out == -100] = np.nan  # set "air" to nan
+# yslice = midx + 1
+# L2out[L2out == -100] = np.nan  # set "air" to nan
+#
+#
+#
+# plt.figure(figsize=(10, 7))
+# plt.suptitle('Smooth Inversion: Depth weight = ' + str(wgtexp))
+# ax = plt.subplot(221)
+# dat1 = mesh.plotSlice(L2out, ax=ax, normal='Z', ind=-16,
+#                       clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
+# plt.plot(np.array([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
+#          np.array([mesh.vectorCCy[yslice], mesh.vectorCCy[yslice]]),
+#          c='gray', linestyle='--')
+# plt.scatter(rxLoc[0:, 0], rxLoc[0:, 1], color='k', s=1)
+# plt.title('Z: ' + str(mesh.vectorCCz[-16]) + ' m')
+# plt.xlabel('Easting (m)')
+# plt.ylabel('Northing (m)')
+# plt.gca().set_aspect('equal', adjustable='box')
+# cb = plt.colorbar(dat1[0], orientation="vertical",
+#                   ticks=np.linspace(vmin, vmax, 4))
+# cb.set_label('MagSusc (SI)')
 
-plt.figure(figsize=(10, 7))
-plt.suptitle('Smooth Inversion: Depth weight = ' + str(wgtexp))
-ax = plt.subplot(221)
-dat1 = mesh.plotSlice(L2out, ax=ax, normal='Z', ind=-16,
-                      clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
-plt.plot(np.array([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-         np.array([mesh.vectorCCy[yslice], mesh.vectorCCy[yslice]]),
-         c='gray', linestyle='--')
-plt.scatter(rxLoc[0:, 0], rxLoc[0:, 1], color='k', s=1)
-plt.title('Z: ' + str(mesh.vectorCCz[-16]) + ' m')
-plt.xlabel('Easting (m)')
-plt.ylabel('Northing (m)')
-plt.gca().set_aspect('equal', adjustable='box')
-cb = plt.colorbar(dat1[0], orientation="vertical",
-                  ticks=np.linspace(vmin, vmax, 4))
-cb.set_label('MagSusc (SI)')
+# ax = plt.subplot(222)
+# dat = mesh.plotSlice(L2out, ax=ax, normal='Z', ind=-27,
+#                      clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
+# plt.plot(np.array([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
+#          np.array([mesh.vectorCCy[yslice], mesh.vectorCCy[yslice]]),
+#          c='gray', linestyle='--')
+# plt.scatter(rxLoc[0:, 0], rxLoc[0:, 1], color='k', s=1)
+# plt.title('Z: ' + str(mesh.vectorCCz[-27]) + ' m')
+# plt.xlabel('Easting (m)')
+# plt.ylabel('Northing (m)')
+# plt.gca().set_aspect('equal', adjustable='box')
+# cb = plt.colorbar(dat1[0], orientation="vertical",
+#                   ticks=np.linspace(vmin, vmax, 4))
+# cb.set_label('MagSusc (SI)')
 
-ax = plt.subplot(222)
-dat = mesh.plotSlice(L2out, ax=ax, normal='Z', ind=-27,
-                     clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
-plt.plot(np.array([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-         np.array([mesh.vectorCCy[yslice], mesh.vectorCCy[yslice]]),
-         c='gray', linestyle='--')
-plt.scatter(rxLoc[0:, 0], rxLoc[0:, 1], color='k', s=1)
-plt.title('Z: ' + str(mesh.vectorCCz[-27]) + ' m')
-plt.xlabel('Easting (m)')
-plt.ylabel('Northing (m)')
-plt.gca().set_aspect('equal', adjustable='box')
-cb = plt.colorbar(dat1[0], orientation="vertical",
-                  ticks=np.linspace(vmin, vmax, 4))
-cb.set_label('MagSusc (SI)')
+# ax = plt.subplot(212)
+# mesh.plotSlice(L2out, ax=ax, normal='Y', ind=yslice,
+#                clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
+# plt.title('Cross Section')
+# plt.xlabel('Easting(m)')
+# plt.ylabel('Elevation')
+# plt.gca().set_aspect('equal', adjustable='box')
+# cb = plt.colorbar(dat1[0], orientation="vertical",
+#                   ticks=np.linspace(vmin, vmax, 4), cmap='bwr')
+# cb.set_label('MagSusc (SI)')
 
-ax = plt.subplot(212)
-mesh.plotSlice(L2out, ax=ax, normal='Y', ind=yslice,
-               clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
-plt.title('Cross Section')
-plt.xlabel('Easting(m)')
-plt.ylabel('Elevation')
-plt.gca().set_aspect('equal', adjustable='box')
-cb = plt.colorbar(dat1[0], orientation="vertical",
-                  ticks=np.linspace(vmin, vmax, 4), cmap='bwr')
-cb.set_label('MagSusc (SI)')
+# # %%
+# # Make plots of Lp model
+# yslice = midx + 1
+# Lpout[Lpout == -100] = np.nan  # set "air" to nan
 
-# %%
-# Make plots of Lp model
-yslice = midx + 1
-Lpout[Lpout == -100] = np.nan  # set "air" to nan
+# plt.figure(figsize=(10, 7))
+# plt.suptitle('Compact Inversion: Depth weight = ' + str(wgtexp) +
+#              ': $\epsilon_p$ = ' + str(round(reg.eps_p[0], 1)) +
+#              ': $\epsilon_q$ = ' + str(round(reg.eps_q[0], 2)))
+# ax = plt.subplot(221)
+# dat = mesh.plotSlice(Lpout, ax=ax, normal='Z', ind=-16,
+#                      clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
+# plt.plot(np.array([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
+#          np.array([mesh.vectorCCy[yslice], mesh.vectorCCy[yslice]]),
+#          c='gray', linestyle='--')
+# plt.scatter(rxLoc[0:, 0], rxLoc[0:, 1], color='k', s=1)
+# plt.title('Z: ' + str(mesh.vectorCCz[-16]) + ' m')
+# plt.xlabel('Easting (m)')
+# plt.ylabel('Northing (m)')
+# plt.gca().set_aspect('equal', adjustable='box')
+# cb = plt.colorbar(dat[0], orientation="vertical",
+#                   ticks=np.linspace(vmin, vmax, 4))
+# cb.set_label('MagSusc (SI)')
 
-plt.figure(figsize=(10, 7))
-plt.suptitle('Compact Inversion: Depth weight = ' + str(wgtexp) +
-             ': $\epsilon_p$ = ' + str(round(reg.eps_p, 1)) +
-             ': $\epsilon_q$ = ' + str(round(reg.eps_q, 2)))
-ax = plt.subplot(221)
-dat = mesh.plotSlice(Lpout, ax=ax, normal='Z', ind=-16,
-                     clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
-plt.plot(np.array([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-         np.array([mesh.vectorCCy[yslice], mesh.vectorCCy[yslice]]),
-         c='gray', linestyle='--')
-plt.scatter(rxLoc[0:, 0], rxLoc[0:, 1], color='k', s=1)
-plt.title('Z: ' + str(mesh.vectorCCz[-16]) + ' m')
-plt.xlabel('Easting (m)')
-plt.ylabel('Northing (m)')
-plt.gca().set_aspect('equal', adjustable='box')
-cb = plt.colorbar(dat[0], orientation="vertical",
-                  ticks=np.linspace(vmin, vmax, 4))
-cb.set_label('MagSusc (SI)')
+# ax = plt.subplot(222)
+# dat = mesh.plotSlice(Lpout, ax=ax, normal='Z', ind=-27,
+#                      clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
+# plt.plot(np.array([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
+#          np.array([mesh.vectorCCy[yslice], mesh.vectorCCy[yslice]]),
+#          c='gray', linestyle='--')
+# plt.scatter(rxLoc[0:, 0], rxLoc[0:, 1], color='k', s=1)
+# plt.title('Z: ' + str(mesh.vectorCCz[-27]) + ' m')
+# plt.xlabel('Easting (m)')
+# plt.ylabel('Northing (m)')
+# plt.gca().set_aspect('equal', adjustable='box')
+# cb = plt.colorbar(dat[0], orientation="vertical",
+#                   ticks=np.linspace(vmin, vmax, 4))
+# cb.set_label('MagSusc (SI)')
 
-ax = plt.subplot(222)
-dat = mesh.plotSlice(Lpout, ax=ax, normal='Z', ind=-27,
-                     clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
-plt.plot(np.array([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-         np.array([mesh.vectorCCy[yslice], mesh.vectorCCy[yslice]]),
-         c='gray', linestyle='--')
-plt.scatter(rxLoc[0:, 0], rxLoc[0:, 1], color='k', s=1)
-plt.title('Z: ' + str(mesh.vectorCCz[-27]) + ' m')
-plt.xlabel('Easting (m)')
-plt.ylabel('Northing (m)')
-plt.gca().set_aspect('equal', adjustable='box')
-cb = plt.colorbar(dat[0], orientation="vertical",
-                  ticks=np.linspace(vmin, vmax, 4))
-cb.set_label('MagSusc (SI)')
-
-ax = plt.subplot(212)
-dat = mesh.plotSlice(Lpout, ax=ax, normal='Y', ind=yslice,
-                     clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
-plt.title('Cross Section')
-plt.xlabel('Easting (m)')
-plt.ylabel('Elevation (m)')
-plt.gca().set_aspect('equal', adjustable='box')
-cb = plt.colorbar(dat[0], orientation="vertical",
-                  ticks=np.linspace(vmin, vmax, 4))
-cb.set_label('MagSusc (SI)')
+# ax = plt.subplot(212)
+# dat = mesh.plotSlice(Lpout, ax=ax, normal='Y', ind=yslice,
+#                      clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
+# plt.title('Cross Section')
+# plt.xlabel('Easting (m)')
+# plt.ylabel('Elevation (m)')
+# plt.gca().set_aspect('equal', adjustable='box')
+# cb = plt.colorbar(dat[0], orientation="vertical",
+#                   ticks=np.linspace(vmin, vmax, 4))
+# cb.set_label('MagSusc (SI)')
 
 # if __name__ == '__main__':
 #     run()
