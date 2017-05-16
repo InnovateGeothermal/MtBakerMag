02af6278758f9c7992663cd120b7625943449bc9
fourndo <fourndo@gmail.com>
2017-05-16 16:18:59 -0700

    Update for Jeff -- Pull latest SimPEG/Mag_Amplitude branch

--- MB_Mag_Coarse_100m_HotSpringsAOI.py
+++ MB_Mag_Coarse_100m_HotSpringsAOI.py
@@ -9,7 +9,6 @@ import os
 import SimPEG.PF as PF
 from SimPEG import Maps, Regularization, Optimization, DataMisfit,\
                    InvProblem, Directives, Inversion, Mesh
-from SimPEG.Utils.io_utils import remoteDownload
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -45,10 +44,12 @@ H0 = (54318, 70.139, 16.063)
 # DOM: Just point to your working directory where your input files are.
 # Since you move them to the current directory, it is just .\
 work_dir = ".\\"
-input_file = "MB_100m_input_file_floor30nT.inp"
+out_dir = "SimPEG_PF_Inv\\"
+input_file = "MB_100m_input_file.inp"
 # %%
 # Read in the input file which included all parameters at once
 # (mesh, topo, model, survey, inv param, etc.)
+os.system('mkdir ' + work_dir+out_dir)
 driver = PF.MagneticsDriver.MagneticsDriver_Inv(work_dir + input_file)
 # %%
 # Now we need to create the survey and model information.
@@ -85,7 +86,7 @@ midx = int(mesh.nCx/2)
 # %%
 # Now that we have a model and a survey we can build the linear system ...
 # Create the forward model operator
-prob = PF.Magnetics.MagneticIntegral(mesh, mapping=staticCells,
+prob = PF.Magnetics.MagneticIntegral(mesh, chiMap=staticCells,
                                   actInd=active)
 prob.solverOpts['accuracyTol'] = 1e-4
 
@@ -97,12 +98,18 @@ survey.pair(prob)
 #                                np.min(mesh.hx)/4.)
 # wr = wr**2.
 # DOM: I recommand just using the sensitivity for the MAG problem
-wr = np.sum(prob.G**2., axis=0)**0.5
+wr = np.sum(prob.F**2., axis=0)**0.5
 wr = (wr/np.max(wr))
 
 # %% Create inversion objects
 reg = Regularization.Sparse(mesh, indActive=active,
                             mapping=staticCells)
+reg.norms = driver.lpnorms
+
+if driver.eps is not None:
+    reg.eps_p = driver.eps[0]
+    reg.eps_q = driver.eps[1]
+
 reg.mref = driver.mref[dynamic]
 reg.cell_weights = wr
 
@@ -113,7 +120,7 @@ opt = Optimization.ProjectedGNCG(maxIter=150, lower=driver.bounds[0],
 
 # Define misfit function (obs-calc)
 dmis = DataMisfit.l2_DataMisfit(survey)
-dmis.Wd = 1./wd
+dmis.W = 1./wd
 
 # create the default L2 inverse problem from the above objects
 invProb = InvProblem.BaseInvProblem(dmis, reg, opt)
@@ -125,140 +132,35 @@ betaest = Directives.BetaEstimate_ByEig()
 # IRLS sets up the Lp inversion problem
 # Set the eps parameter parameter in Line 11 of the
 # input file based on the distribution of model (DEFAULT = 95th %ile)
-IRLS = Directives.Update_IRLS(norms=driver.lpnorms, eps=driver.eps,
-                              f_min_change=1e-2, maxIRLSiter=20,
+IRLS = Directives.Update_IRLS(f_min_change=1e-2, maxIRLSiter=20,
                               minGNiter=5)
 
 # Preconditioning refreshing for each IRLS iteration
-update_Jacobi = Directives.Update_lin_PreCond()
+update_Jacobi = Directives.UpdatePreCond()
 
+saveModel = Directives.SaveUBCModelEveryIteration(mapping=activeMap)
+saveModel.fileName = work_dir + out_dir + 'MagSus'
 
 # Create combined the L2 and Lp problem
 inv = Inversion.BaseInversion(invProb,
-                              directiveList=[IRLS, update_Jacobi, betaest])
+                              directiveList=[betaest, IRLS, update_Jacobi, saveModel])
 
 # %%
 # Run L2 and Lp inversion
 mrec = inv.run(mstart)
 
 # %%
-# if plotIt:
 # Plot observed data
 PF.Magnetics.plot_obs_2D(rxLoc, d, 'Observed Data')
 
 # %%
 # Write output model and data files and print misft stats.
 
-# reconstructing l2 model mesh with air cells and active dynamic cells
-L2out = activeMap * reg.l2model
+if getattr(invProb, 'l2model', None) is not None:
+    # reconstructing l2 model mesh with air cells and active dynamic cells
+    L2out = activeMap * invProb.l2model
+    Mesh.TensorMesh.writeModelUBC(mesh, work_dir + 'L2_model.sus', L2out)
 
 # reconstructing lp model mesh with air cells and active dynamic cells
 Lpout = activeMap*mrec
-
-Mesh.TensorMesh.writeModelUBC(mesh, work_dir + 'L2_model.sus', L2out)
 Mesh.TensorMesh.writeModelUBC(mesh, work_dir + 'Lp_model.sus', Lpout)
-
-# %%
-# Plot out sections and histograms of the smooth l2 model.
-# The ind= parameter is the slice of the model from top down.
-# yslice = midx + 1
-# L2out[L2out == -100] = np.nan  # set "air" to nan
-#
-#
-#
-# plt.figure(figsize=(10, 7))
-# plt.suptitle('Smooth Inversion: Depth weight = ' + str(wgtexp))
-# ax = plt.subplot(221)
-# dat1 = mesh.plotSlice(L2out, ax=ax, normal='Z', ind=-16,
-#                       clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
-# plt.plot(np.array([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-#          np.array([mesh.vectorCCy[yslice], mesh.vectorCCy[yslice]]),
-#          c='gray', linestyle='--')
-# plt.scatter(rxLoc[0:, 0], rxLoc[0:, 1], color='k', s=1)
-# plt.title('Z: ' + str(mesh.vectorCCz[-16]) + ' m')
-# plt.xlabel('Easting (m)')
-# plt.ylabel('Northing (m)')
-# plt.gca().set_aspect('equal', adjustable='box')
-# cb = plt.colorbar(dat1[0], orientation="vertical",
-#                   ticks=np.linspace(vmin, vmax, 4))
-# cb.set_label('MagSusc (SI)')
-
-# ax = plt.subplot(222)
-# dat = mesh.plotSlice(L2out, ax=ax, normal='Z', ind=-27,
-#                      clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
-# plt.plot(np.array([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-#          np.array([mesh.vectorCCy[yslice], mesh.vectorCCy[yslice]]),
-#          c='gray', linestyle='--')
-# plt.scatter(rxLoc[0:, 0], rxLoc[0:, 1], color='k', s=1)
-# plt.title('Z: ' + str(mesh.vectorCCz[-27]) + ' m')
-# plt.xlabel('Easting (m)')
-# plt.ylabel('Northing (m)')
-# plt.gca().set_aspect('equal', adjustable='box')
-# cb = plt.colorbar(dat1[0], orientation="vertical",
-#                   ticks=np.linspace(vmin, vmax, 4))
-# cb.set_label('MagSusc (SI)')
-
-# ax = plt.subplot(212)
-# mesh.plotSlice(L2out, ax=ax, normal='Y', ind=yslice,
-#                clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
-# plt.title('Cross Section')
-# plt.xlabel('Easting(m)')
-# plt.ylabel('Elevation')
-# plt.gca().set_aspect('equal', adjustable='box')
-# cb = plt.colorbar(dat1[0], orientation="vertical",
-#                   ticks=np.linspace(vmin, vmax, 4), cmap='bwr')
-# cb.set_label('MagSusc (SI)')
-
-# # %%
-# # Make plots of Lp model
-# yslice = midx + 1
-# Lpout[Lpout == -100] = np.nan  # set "air" to nan
-
-# plt.figure(figsize=(10, 7))
-# plt.suptitle('Compact Inversion: Depth weight = ' + str(wgtexp) +
-#              ': $\epsilon_p$ = ' + str(round(reg.eps_p[0], 1)) +
-#              ': $\epsilon_q$ = ' + str(round(reg.eps_q[0], 2)))
-# ax = plt.subplot(221)
-# dat = mesh.plotSlice(Lpout, ax=ax, normal='Z', ind=-16,
-#                      clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
-# plt.plot(np.array([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-#          np.array([mesh.vectorCCy[yslice], mesh.vectorCCy[yslice]]),
-#          c='gray', linestyle='--')
-# plt.scatter(rxLoc[0:, 0], rxLoc[0:, 1], color='k', s=1)
-# plt.title('Z: ' + str(mesh.vectorCCz[-16]) + ' m')
-# plt.xlabel('Easting (m)')
-# plt.ylabel('Northing (m)')
-# plt.gca().set_aspect('equal', adjustable='box')
-# cb = plt.colorbar(dat[0], orientation="vertical",
-#                   ticks=np.linspace(vmin, vmax, 4))
-# cb.set_label('MagSusc (SI)')
-
-# ax = plt.subplot(222)
-# dat = mesh.plotSlice(Lpout, ax=ax, normal='Z', ind=-27,
-#                      clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
-# plt.plot(np.array([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-#          np.array([mesh.vectorCCy[yslice], mesh.vectorCCy[yslice]]),
-#          c='gray', linestyle='--')
-# plt.scatter(rxLoc[0:, 0], rxLoc[0:, 1], color='k', s=1)
-# plt.title('Z: ' + str(mesh.vectorCCz[-27]) + ' m')
-# plt.xlabel('Easting (m)')
-# plt.ylabel('Northing (m)')
-# plt.gca().set_aspect('equal', adjustable='box')
-# cb = plt.colorbar(dat[0], orientation="vertical",
-#                   ticks=np.linspace(vmin, vmax, 4))
-# cb.set_label('MagSusc (SI)')
-
-# ax = plt.subplot(212)
-# dat = mesh.plotSlice(Lpout, ax=ax, normal='Y', ind=yslice,
-#                      clim=(vmin, vmax), pcolorOpts={'cmap': 'bwr'})
-# plt.title('Cross Section')
-# plt.xlabel('Easting (m)')
-# plt.ylabel('Elevation (m)')
-# plt.gca().set_aspect('equal', adjustable='box')
-# cb = plt.colorbar(dat[0], orientation="vertical",
-#                   ticks=np.linspace(vmin, vmax, 4))
-# cb.set_label('MagSusc (SI)')
-
-# if __name__ == '__main__':
-#     run()
-#     plt.show()
